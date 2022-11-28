c5027d6c82beff9b1cc63d6dcd4b7336d5d2f69a
NARESH CHOUDHARY <107427909+Nareshchoudhary02@users.noreply.github.com>
2022-11-28 12:50:17 +0530

    Update dicee.js

--- dicee.js
+++ dicee.js
@@ -1,13 +1,13 @@
 var randomNumber1 = Math.floor(Math.random()*6) + 1;
 
-var randomDiceImage1 = "/images/dice" + randomNumber1 + ".png";
+var randomDiceImage1 = "images/dice" + randomNumber1 + ".png";
 
 document.querySelector(".img1").setAttribute("src" , randomDiceImage1);
 
 
 var randomNumber2 = Math.floor(Math.random()*6)+1;
 
-var randomDiceImage2 =  "/images/dice" + randomNumber2 + ".png";
+var randomDiceImage2 =  "images/dice" + randomNumber2 + ".png";
 
 document.querySelector(".img2").setAttribute("src", randomDiceImage2);
 
